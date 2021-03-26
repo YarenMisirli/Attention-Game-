a4cb6eef81ddf7b9cb407b9398df0e8780a004b9
YarenMisirli <81428542+YarenMisirli@users.noreply.github.com>
2021-03-26 18:25:17 +0300

    Attention Game
    Software Engineering dersi için Unity uygulaması ile oluşturduğum Attention Game oyun projem

---
+++ Software A.Game/Assets/PermanentUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PermanentUI : MonoBehaviour
+{
+    public int cherries = 0;
+    public int health = 3;
+    public TextMeshProUGUI cherryText;
+    public Text healthAmount;
+
+    public static PermanentUI perm;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (!perm)
+        {
+            perm = this;
+        }
+        else
+            Destroy(gameObject);
+        
+    }
+    public void Reset()
+    {
+        cherries = 0;
+        cherryText.text = cherries.ToString();
+    }
+}
---
+++ Software A.Game/Assets/Question.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Question : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource x;
+    [SerializeField] private AudioSource y;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            x.Play();
+            StartCoroutine(waitForSound());
+            
+
+        }
+        else if (Input.GetKey(KeyCode.B))
+        {
+            y.Play();
+            StartCoroutine(waitForSoundCorrectAnswer());
+            
+
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            x.Play();
+            StartCoroutine(waitForSound());
+            
+
+        }
+
+        
+    }
+
+    IEnumerator waitForSound()
+    {
+        while (x.isPlaying)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene("GameOver");
+    }
+
+    IEnumerator waitForSoundCorrectAnswer()
+    {
+        while (y.isPlaying)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene("TheEnd");
+    }
+}
